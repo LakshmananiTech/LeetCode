d6de6bcb1324211ad242c2df74277fe12a2aead3
Lakshmanan, iTech, Thanjavur <lakshhmanan@yahoo.com>
2025-06-06 12:30:11 +0530

    Time: 12 ms (0.07%), Space: 26.9 MB (7.36%) - LeetHub

---
+++ 2715-k-items-with-the-maximum-sum/2715-k-items-with-the-maximum-sum.c
@@ -0,0 +1,34 @@
+int kItemsWithMaximumSum(int n, int nz, int no, int k) {
+   int i,s,j,c,z;
+   j=0;
+   int *a=(int*)malloc(10000*sizeof(int));
+   for(i=1;i<=n;i++)
+   {
+    a[j]=1;
+    j++;
+   } 
+      for(i=1;i<=nz;i++)
+   {
+    a[j]=0;
+    j++;
+   } 
+      for(i=1;i<=no;i++)
+   {
+    a[j]=-1;
+    j++;
+   } 
+   for(i=0;i<j;i++)
+   {
+    for(z=i+1;z<j;z++)
+    if(a[i]<a[z])
+    {
+        c=a[i];
+        a[i]=a[z];
+        a[z]=c;
+    }
+   }
+   s=0;
+   for(i=0;i<k;i++)
+   s=s+a[i];
+   return s;
+}
