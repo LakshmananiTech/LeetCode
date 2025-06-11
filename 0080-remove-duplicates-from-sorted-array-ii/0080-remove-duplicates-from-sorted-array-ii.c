ee32695ad773d8bea7586a55bfc463c11862f377
Lakshmanan, iTech, Thanjavur <lakshhmanan@yahoo.com>
2025-06-11 16:35:00 +0530

    Time: 800 ms (5.02%), Space: 14.3 MB (88.56%) - LeetHub

---
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.c
@@ -0,0 +1,14 @@
+int removeDuplicates(int* a, int n) {
+    int i,j,f,k,c;
+    k=0;
+    for(i=0;i<n;i++)
+    {
+            c=0;
+            for(j=i;j<n;j++)
+            if(a[i]==a[j])
+            c++;
+        if(c<=2)
+        a[k++]=a[i];
+    }
+    return k;
+}
