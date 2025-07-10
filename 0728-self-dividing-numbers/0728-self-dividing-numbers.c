5455ebb7350064fe397e9b73d74684e67deebf13
Lakshmanan, iTech, Thanjavur <lakshhmanan@yahoo.com>
2025-07-10 16:22:23 +0530

    Time: 0 ms (100%), Space: 10.8 MB (6.25%) - LeetHub

--- 0728-self-dividing-numbers/0728-self-dividing-numbers.c
+++ 0728-self-dividing-numbers/0728-self-dividing-numbers.c
@@ -1,30 +1,33 @@
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
-int* selfDividingNumbers(int l, int ri, int* returnSize) {
-    int r,f,i,m,k;
-    k=0;
-   int* a =(int*)malloc((10000)*sizeof(int));
-  for(i=l;i<=ri;i++)
+int* selfDividingNumbers(int left, int right, int* returnSize) 
+{
+    int *a = malloc(100000*sizeof(int));
+    int i,m,n,f,r,c=0;
+  for(i=left;i<=right;i++)  
   {
-  m=i;
-  f=0;
-  while(m!=0)
-  {
-    r=m%10;
-    if(r==0 || i%r!=0)
+    m = i;
+    n = i;
+    f = 1;
+    while(m!=0)
     {
-    f=1;
-    break;
+      r=m%10;
+      if(r==0 || n%r!=0)
+      {
+        f = 0;
+        break;
+      }
+        m = m/10;
     }
-    m=m/10;
-  }  
-  if(f==0)
-  {
-    a[k]=i;
-    k++;
+    if(f==1)
+    a[c++] = i;
   }
-  }
- *returnSize=k;
- return a;                                
+
+  *returnSize = c;
+  return a;
+
+
+
+
 }
