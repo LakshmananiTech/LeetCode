e140f07a3080c2615f2ff4007eb9fdc63eff8dd4
Lakshmanan, iTech, Thanjavur <lakshhmanan@yahoo.com>
2025-07-21 12:05:40 +0530

    Time: 1 ms (88.52%), Space: 102.4 MB (10.53%) - LeetHub

--- 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.c
+++ 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.c
@@ -1,26 +1,29 @@
 /**
+    
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* pivotArray(int* nums, int numsSize, int pivot, int* returnSize) {
-    *returnSize = numsSize;
     int n = numsSize;
-    int *b = (int*)malloc(numsSize*sizeof(int));
-    int c=0;
-    int c1=0;
-    for(int i=0;i<n;i++)
-    if(nums[i] < pivot)
-    c++;
-    else if(nums[i] == pivot)
-    c1++;
+    *returnSize = n;
+    int *a = (int *)malloc(n * sizeof(int));
+    int *b = (int *)malloc(n * sizeof(int));
+    int *c = (int *)malloc(n * sizeof(int));
     int p=0;
-    int q = c+c1;
+    int q=0;
+    int r=0;
     for(int i=0;i<n;i++)
     if(nums[i] < pivot)
-    b[p++] = nums[i];
-    else if(nums[i] > pivot)
-    b[q++]= nums[i];
-    for(int i = 1;i<=c1;i++)
-    b[c++] = pivot;
-    return b;
+    a[p++] = nums[i];
+    else if(nums[i] == pivot)
+    b[q++] = nums[i];
+    else
+    c[r++] = nums[i];
+    for(int i=0;i<q;i++)
+    a[p++] = b[i];
+    for(int i=0;i<r;i++)
+    a[p++] = c[i];
+    return a;
+    
+
     
 }
