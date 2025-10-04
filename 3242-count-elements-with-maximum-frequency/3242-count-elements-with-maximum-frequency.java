954968401deb63e0d35640f85ab2344fb909342f
Lakshmanan, iTech, Thanjavur <lakshhmanan@yahoo.com>
2025-10-04 14:06:15 +0530

    Time: 3 ms (23.35%), Space: 42.8 MB (13.22%) - LeetHub

--- 3242-count-elements-with-maximum-frequency/3242-count-elements-with-maximum-frequency.java
+++ 3242-count-elements-with-maximum-frequency/3242-count-elements-with-maximum-frequency.java
@@ -1,14 +1,24 @@
 class Solution {
     public int maxFrequencyElements(int[] nums) {
-        Map<Integer,Integer> map = new HashMap<>();
-        for(int k : nums)
-        map.merge(k,1,Integer::sum);
 
-
-        java.util.Collection<Integer> list1 = map.values();
-        int max = list1.stream().max((a,b) -> a.compareTo(b)).get();
-        long c = list1.stream().filter(k -> k == max).count();        
-        return (int) c * max;
+        Map<Integer, Integer> hmap = new HashMap<>();
+        for(Integer k: nums)
+        hmap.put(k, hmap.getOrDefault(k,0)+1);
+        java.util.Collection c = hmap.values();
+        List<Integer> list = new ArrayList<>(c);
+        int big = list.get(0);
+        for(Integer k: list)
+        if(k>big)
+        big = k;
+        int s = 0;
+        for(Map.Entry<Integer, Integer> e: hmap.entrySet())
+        {
+            int k = e.getKey();
+            int v = e.getValue();
+            if(v == big)
+            s++;
         
+        }
+        return s*big;
     }
 }
