40a57784d78466f865a7dbff7cd560d2cda8f436
Lakshmanan, iTech, Thanjavur <lakshhmanan@yahoo.com>
2026-03-01 15:00:11 +0530

    Time: 4 ms (15.98%), Space: 43.8 MB (19.78%) - LeetHub

---
+++ 1816-truncate-sentence/1816-truncate-sentence.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String truncateSentence(String s, int k) {
+        String t[] = s.split(" ");
+        String ans = "";
+        for(int i=0;i<k;i++)
+        ans = ans + t[i]+ " ";
+        return ans.trim();
+    }
+}
