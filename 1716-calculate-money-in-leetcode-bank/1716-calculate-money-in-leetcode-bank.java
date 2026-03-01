1aff8fd45b50d0d4d8a5b71d58e8721cb71016b6
Lakshmanan, iTech, Thanjavur <lakshhmanan@yahoo.com>
2026-03-01 19:23:05 +0530

    Time: 0 ms (100%), Space: 42.2 MB (47.13%) - LeetHub

---
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int totalMoney(int n) {
+        int s  = 0;
+        if(n < 7)
+        {for(int i = 1;i<= n;i++)
+        s = s + i;
+        return s;
+        }
+        int q = n / 7;
+        int r = n % 7;
+        int a = 28;
+        for(int i=1;i<=q;i++)
+        s = s + a + (7 * (i-1));
+        for(int i=1;i<=r;i++)
+        s = s + (i +(q));
+        return s;
+
+        
+    }
+}
