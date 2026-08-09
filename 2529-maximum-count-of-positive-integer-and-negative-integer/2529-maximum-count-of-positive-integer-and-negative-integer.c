c6e86537364dd91569569ac52485b93afd62b261
Lakshmanan, iTech, Thanjavur <lakshhmanan@yahoo.com>
2026-08-09 15:39:37 +0530

    Time: 0 ms (100%), Space: 9.5 MB (49.23%) - LeetHub

--- 2529-maximum-count-of-positive-integer-and-negative-integer/2529-maximum-count-of-positive-integer-and-negative-integer.c
+++ 2529-maximum-count-of-positive-integer-and-negative-integer/2529-maximum-count-of-positive-integer-and-negative-integer.c
@@ -2,12 +2,12 @@ int maximumCount(int* nums, int numsSize) {
     int c1=0,c2=0,i;
     for(i=0;i<numsSize;i++)
     {
-        if(nums[i]>0)
-        c1++;
-        else if(nums[i]<0)
-        c2++;
-        else
+        if(nums[i]==0)
         continue;
+        else if(nums[i]>0)
+        c1++;
+        else
+        c2++;
     }
     if(c1>=c2)
     return c1;
